11dc561d18078eae4acf808260413d9805c383dc
Hyeonchul <wkdgus1136@naver.com>
2023-09-19 22:28:19 +0900

    [f/d] 스프링 프로젝트 업로드

---
+++ src/main/java/com/example/texttopicture/TextToPictureApplication.java
@@ -0,0 +1,13 @@
+package com.example.texttopicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextToPictureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextToPictureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/texttopicture/TextToPictureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.texttopicture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextToPictureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
