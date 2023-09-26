b0e4ec7cc8dee1589abb5f8012d0bc61065d12c7
bee <bee@outlier.kr>
2023-09-26 14:29:53 +0900

    build.Gradle 의존성 변경 및 Swagger 문서 추가

---
+++ src/main/java/com/example/texttopicture/Config/Swagger/SwaggerController.java
@@ -0,0 +1,2 @@
+package com.example.texttopicture.Config.Swagger;public class SwaggerController {
+}
--- src/main/java/com/example/texttopicture/text.java
+++ src/main/java/com/example/texttopicture/text.java
@@ -1,23 +1,29 @@
 package com.example.texttopicture;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+
+@Tag( name = "posts", description = "테스트용 API")
 @RestController
+@RequestMapping("/api")
 public class text {
 
-    // http://localhost:8080/test
+    // http://localhost:8080/api/testapi/test2
     @CrossOrigin
-    @GetMapping("/test")
+    @GetMapping("/test2")
     public String test(@RequestParam("text") String text ) {
         System.out.println("text = " + text);
 
         return text;
     }
 
+
+
     @CrossOrigin
     // http://localhost:8080/test2
-    @GetMapping("/test2")
+    @GetMapping("/test1")
     public String test2() {
 
 
